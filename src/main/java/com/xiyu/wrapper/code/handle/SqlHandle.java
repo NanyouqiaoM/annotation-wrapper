dc854a32039a48bf1b38d1f937bea5f139874cbc
caowu <wu_cao@bjdv.com>
2024-06-18 14:57:07 +0800

    SqlHandle新增list类型支持和sql注入校验

--- src/main/java/com/xiyu/wrapper/code/handle/SqlHandle.java
+++ src/main/java/com/xiyu/wrapper/code/handle/SqlHandle.java
@@ -2,8 +2,14 @@ package com.xiyu.wrapper.code.handle;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.sql.SqlInjectionUtils;
 import com.xiyu.wrapper.code.annotations.Wrapper;
 import com.xiyu.wrapper.code.enums.SqlType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.baomidou.mybatisplus.core.toolkit.StringPool.*;
 
 /**
  * @author caowu
@@ -35,6 +41,10 @@ public class SqlHandle implements Handle<String> {
         if (obj == null) {
             return null;
         }
+        if (obj instanceof List) {
+            List<?> list = (List<?>) obj;
+            return LEFT_BRACKET + list.stream().map(Object::toString).collect(Collectors.joining(COMMA)) + RIGHT_BRACKET;
+        }
         return String.valueOf(obj);
     }
 
@@ -51,6 +61,9 @@ public class SqlHandle implements Handle<String> {
     public <T> QueryWrapper<T> apply(QueryWrapper<T> queryWrapper, String column, String value, SqlType sqlType, Wrapper wrapper) {
         if (StringUtils.isEmpty(wrapper.extend())) {
             throw new RuntimeException("SqlHandle->apply->wrapper.extend is null");
+        }
+        if (SqlInjectionUtils.check(value)) {
+            throw new RuntimeException("SqlHandle->apply->value is sql injection");
         }
         String sql = String.format(wrapper.extend(), value);
         switch (sqlType) {
